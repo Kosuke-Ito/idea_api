e4275e6c034080161bc7e65e35b639a0049cd3b7
Kosuke Ito <keree.it.owl.uk@gmail.com>
2021-08-13 16:46:38 +0900

    アイデアモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_071143) do
+ActiveRecord::Schema.define(version: 2021_08_13_073834) do
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -19,4 +19,13 @@ ActiveRecord::Schema.define(version: 2021_08_13_071143) do
     t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
+  create_table "ideas", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "categories_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["categories_id"], name: "index_ideas_on_categories_id"
+  end
+
+  add_foreign_key "ideas", "categories", column: "categories_id"
 end
