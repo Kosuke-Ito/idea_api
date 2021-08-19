6569cad418c0c885e4f527508ab98163ad152ac7
Kosuke Ito <keree.it.owl.uk@gmail.com>
2021-08-19 11:06:10 +0900

    idea request spec 追加

---
+++ spec/requests/ideas_api_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+describe 'Ideas API', type: :request do
+  let(:category) { FactoryBot.create(:category) } 
+
+  describe "get idea" do
+
+    it "blank category_name" do
+      get ideas_path
+      expect(response).to  have_http_status(201)
+    end
+    
+    context "presence params category_name" do
+
+      it "category_name presence in db" do
+        FactoryBot.create(:idea, body: 'サンプル1', category_id: category.id)
+        FactoryBot.create(:idea, body: 'サンプル2', category_id: category.id)
+        get ideas_path, params: { category_name: category.name }
+        data = JSON.parse(response.body)["data"]
+        expect(data.size).to eq(2)
+        expect(response).to have_http_status(201)
+      end
+      
+      it "category_name blank in db" do
+        FactoryBot.create(:idea)
+        get ideas_path, params: { category_name: 'DBにないカテゴリ名' }
+        expect(response).to have_http_status(404)
+      end
+
+    end
+
+  end
+  
+  describe "create a idea" do
+
+    context "presence params category_name" do
+      
+      it 'presence category_name in db' do
+        post ideas_path, params: { body: 'カテゴリー既にある', category_name: category.name }
+        expect(response).to have_http_status(201)
+        expect(Idea.all.count).to eq(1)
+      end
+
+      it "blank category_name in db" do
+        expect{
+          post ideas_path, params: { body: 'カテゴリも一緒に作られる', category_name: 'カテゴリー' }
+        }.to change{ Category.all.size }.from(0).to(1)
+        expect(Idea.find_by(body: 'カテゴリも一緒に作られる')).to be_present
+      end
+
+      it 'blank idea body' do
+        post ideas_path, params: { body: nil, category_name: 'カテゴリー' }
+        expect(response).to have_http_status(422)
+      end
+
+    end
+
+    it 'blank category_name' do
+      post ideas_path, params: { body: 'カテゴリなし', category_name: nil }
+      expect(response).to have_http_status(422)
+    end
+
+  end
+
+end
